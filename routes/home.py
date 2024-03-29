2cd08b7b856ab0e74061675a26febc36f3be6bfd
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-03-29 14:36:29 +0800

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
-home_bp = Blueprint('home', __name__)
+home_bp = Blueprint("home", __name__)
 
 
-@home_bp.route('/')
+@home_bp.route("/")
 def index():
-    return render_template('home.html')
+    return render_template("home.html")
+
+
+@home_bp.route("/favicon.ico")
+def favicon_route():
+    return send_from_directory("static", "favicon.ico", mimetype="image/vnd.microsoft.icon")
