1b41f521b768f58f3094f35377e2b7ec5558c6e4
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-03-29 14:31:33 +0800

    Update user.py

--- processors/user.py
+++ processors/user.py
@@ -4,5 +4,4 @@ from models.user_tab import get_users
 
 def get_user_list():
     users = get_users()
-    usernames = [user["username"] for user in users]
-    return usernames
+    return [{"name": user["name"], "email": user["email"]} for user in users]
