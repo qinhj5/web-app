3b32ad751e850e2037130c71f7643046f150f843
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-03-29 18:34:28 +0800

    Update user_tab.py

--- models/user_tab.py
+++ models/user_tab.py
@@ -7,7 +7,7 @@ from sqlalchemy.dialects.mysql import INTEGER, BIGINT, VARCHAR, TEXT, TINYINT
 
 class UserTab(db.Model):
     id = db.Column(INTEGER, primary_key=True, autoincrement=True)
-    user_id = db.Column(BIGINT(unsigned=True), unique=True, nullable=False)
+    user_id = db.Column(VARCHAR(length=32), unique=True, nullable=False)
     name = db.Column(VARCHAR(length=32), nullable=False)
     email = db.Column(VARCHAR(length=64), unique=True, nullable=False)
     credential = db.Column(TEXT, nullable=False)
@@ -56,28 +56,28 @@ def update(user_info):
 
 def get_user_by_user_id(user_id):
     try:
-        user = UserTab.query.filter(UserTab.user_id == user_id).one()
+        user = UserTab.query.filter(UserTab.user_id == user_id).first()
     except Exception as e:
         print(f"{e}\n{traceback.format_exc()}")
-        return {}
+        return None
     else:
-        return user.to_dict()
+        return user.to_dict() if user else None
 
 
 def init_users():
     user_list = [
-        {"user_id": 1, "name": "Pony Ma", "email": "pony.ma@gmail.com", "credential": "credential.1"},
-        {"user_id": 2, "name": "Jack Ma", "email": "jack.ma@gmail.com", "credential": "credential.2"},
-        {"user_id": 3, "name": "Stephen Chow", "email": "stephen.chow@gmail.com", "credential": "credential.3"},
-        {"user_id": 4, "name": "Elon Musk", "email": "elon.musk@gmail.com", "credential": "credential.4"},
-        {"user_id": 5, "name": "Allen Walker", "email": "allen.walker@gmail.com", "credential": "credential.5"},
-        {"user_id": 6, "name": "Michael Jackson", "email": "michael.jackson@gmail.com", "credential": "credential.6"},
-        {"user_id": 7, "name": "Albert Einstein", "email": "albert.einstein@gmail.com", "credential": "credential.7"},
-        {"user_id": 8, "name": "Steve Jobs", "email": "steve.jobs@gmail.com", "credential": "credential.8"},
-        {"user_id": 9, "name": "Nelson Mandela", "email": "nelson.mandela@gmail.com", "credential": "credential.9"},
-        {"user_id": 10, "name": "Marie Curie", "email": "marie.curie@gmail.com", "credential": "credential.10"},
-        {"user_id": 11, "name": "Isaac Newton", "email": "isaac.newton@gmail.com", "credential": "credential.11"},
-        {"user_id": 12, "name": "Nikola Tesla", "email": "nikola.tesla@gmail.com", "credential": "credential.12"},
+        {"user_id": "1", "name": "Pony Ma", "email": "pony.ma@gmail.com", "credential": "credential.1"},
+        {"user_id": "2", "name": "Jack Ma", "email": "jack.ma@gmail.com", "credential": "credential.2"},
+        {"user_id": "3", "name": "Stephen Chow", "email": "stephen.chow@gmail.com", "credential": "credential.3"},
+        {"user_id": "4", "name": "Elon Musk", "email": "elon.musk@gmail.com", "credential": "credential.4"},
+        {"user_id": "5", "name": "Allen Walker", "email": "allen.walker@gmail.com", "credential": "credential.5"},
+        {"user_id": "6", "name": "Michael Jackson", "email": "michael.jackson@gmail.com", "credential": "credential.6"},
+        {"user_id": "7", "name": "Albert Einstein", "email": "albert.einstein@gmail.com", "credential": "credential.7"},
+        {"user_id": "8", "name": "Steve Jobs", "email": "steve.jobs@gmail.com", "credential": "credential.8"},
+        {"user_id": "9", "name": "Nelson Mandela", "email": "nelson.mandela@gmail.com", "credential": "credential.9"},
+        {"user_id": "10", "name": "Marie Curie", "email": "marie.curie@gmail.com", "credential": "credential.10"},
+        {"user_id": "11", "name": "Isaac Newton", "email": "isaac.newton@gmail.com", "credential": "credential.11"},
+        {"user_id": "12", "name": "Nikola Tesla", "email": "nikola.tesla@gmail.com", "credential": "credential.12"},
     ]
     for user in user_list:
         add(user)
