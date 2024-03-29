2e0918beeb2a8fee3fbbe585b70fb7f9d59041f0
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-03-29 14:36:19 +0800

    Delete routes/favicon.py

--- routes/favicon.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import Blueprint, send_from_directory
-
-favicon_bp = Blueprint("favicon", __name__)
-
-
-@favicon_bp.route("/favicon.ico")
-def favicon_route():
-    return send_from_directory("static", "favicon.ico", mimetype="image/vnd.microsoft.icon")
