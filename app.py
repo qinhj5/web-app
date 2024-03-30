9e4d5bdf043f1e73202d96ebf550a0cace30f6b7
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-03-31 00:06:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import os
 import json
 from models import db
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app():
     root_dir = os.path.abspath(os.path.dirname(__file__))
     flask_app = Flask(__name__)
+    CORS(flask_app)
+    flask_app.config["CORS_HEADERS"] = "Authorization"
 
     mysql_config_path = os.path.abspath(os.path.join(root_dir, "config/mysql.json"))
     with open(mysql_config_path) as f:
